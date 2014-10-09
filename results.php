f5a8a888ecaaff588ea253059ba8660fbc64f4ba
Mike Krejci <mike@maplight.org>
2014-10-09 14:04:07 -0700

    Added blockquote to default search message.

--- results.php
+++ results.php
@@ -1,5 +1,5 @@
 <?php
   function build_results_table () {
-    echo "<P>&nbsp;</P><B>Search political contributions from 2001 through the present, using the controls on the left.</B>";
+    echo "<P>&nbsp;</P><BLOCKQUOTE><B>Search political contributions from 2001 through the present, using the controls on the left.</B></BLOCKQUOTE>";
   }
 ?>
