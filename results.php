07bae468aa24c09c14fda5e75006f124cc4e1825
Mike Krejci <mike@maplight.org>
2014-10-09 14:24:13 -0700

    TECHPROJECTS-108 : Display post array

--- results.php
+++ results.php
@@ -1,5 +1,11 @@
 <?php
   function build_results_table () {
-    echo "<P>&nbsp;</P><BLOCKQUOTE><B>Search political contributions from 2001 through the present, using the controls on the left.</B></BLOCKQUOTE>";
+    if (! isset ($_POST["contributor"])) {
+      # No form search yet
+      echo "<P>&nbsp;</P><BLOCKQUOTE><B>Search political contributions from 2001 through the present, using the controls on the left.</B></BLOCKQUOTE>";
+    } else {
+      # Form search entered
+      echo "<PRE>"; print_r ($_POST); echo "</PRE>";
+    }
   }
 ?>
--- sidebar.php
+++ sidebar.php
@@ -2,29 +2,29 @@
   # Search form HTML
 
   function build_sidebar_form () {
-    echo "<FORM>";
+    echo "<FORM METHOD=POST>";
 
     echo "<B STYLE=\"font-size:1.6em;\">Advanced Search</B><P>";
     echo "<P><INPUT ID=search TYPE=SUBMIT VALUE=\"Search\"></P>";
 
     echo "<B STYLE=\"font-size:1.2em; line-height:200%;\">Contributions From:</B><BR>";
     echo "Contributor name contains:<BR><INPUT TYPE=TEXT NAME=contributor STYLE=\"width:95%;\"><BR STYLE=\"line-height:190%;\">";
-    echo "Contributor Location:<BR><SELECT MULTIPLE NAME=location_list ID=location_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All states</OPTION>";
+    echo "Contributor Location:<BR><SELECT MULTIPLE NAME=location_list[] ID=location_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All states</OPTION>";
     fill_state_list ();
     echo "</SELECT><HR STYLE=\"width:98%;\">";
 
     echo "<B STYLE=\"font-size:1.2em;\">Contributions To:</B><BR>";
     echo "<B STYLE=\"line-height:160%;\">Candidates:</B><BR>";
     echo "Search candidates:<BR><INPUT TYPE=TEXT NAME=search_candidates ID=search_candidates STYLE=\"width:95%;\" onkeyup=\"filter_candidates_list();\"><BR>";
-    echo "<SELECT MULTIPLE NAME=candidates_list ID=candidates_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All candidates</OPTION>";
+    echo "<SELECT MULTIPLE NAME=candidates_list[] ID=candidates_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All candidates</OPTION>";
     $js_candidates = fill_candidate_names ();
     echo "</SELECT><BR STYLE=\"line-height:190%;\">";
-    echo "Office sought<BR><SELECT MULTIPLE NAME=office_list ID=office_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All offices</OPTION>";
+    echo "Office sought<BR><SELECT MULTIPLE NAME=office_list[] ID=office_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All offices</OPTION>";
     fill_offices_sought ();
     echo "</SELECT><P>";
 
     echo "<B STYLE=\"line-height:160%;\">Ballot Measures:</B><BR>";
-    echo "Select election<BR><SELECT MULTIPLE NAME=elections_list ID=election_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All elections</OPTION>";
+    echo "Select election<BR><SELECT MULTIPLE NAME=elections_list[] ID=election_list STYLE=\"width:95%;\"><OPTION VALUE=\"ALL\" SELECTED>-- All elections</OPTION>";
     fill_elections ();
     echo "</SELECT><BR STYLE=\"line-height:190%;\">";
     echo "Search propositions:<BR><INPUT TYPE=TEXT NAME=search_propositions ID=search_propositions STYLE=\"width:95%;\" onkeyup=\"filter_propositions_list();\"><BR>";
@@ -32,9 +32,9 @@
     $js_propositions = fill_propositions ();
     echo "</SELECT><BR STYLE=\"line-height:190%;\">";
     echo "Support & Oppose<BR>";
-    echo "<INPUT TYPE=CHECKBOX CHECKED> Support<BR>";
-    echo "<INPUT TYPE=CHECKBOX CHECKED> Oppose<BR>";
-    echo "<INPUT TYPE=CHECKBOX> Exclude contributions between allied committees<P>";
+    echo "<INPUT NAME=support TYPE=CHECKBOX CHECKED> Support<BR>";
+    echo "<INPUT NAME=oppose TYPE=CHECKBOX CHECKED> Oppose<BR>";
+    echo "<INPUT NAME=exclude TYPE=CHECKBOX> Exclude contributions between allied committees<P>";
 
     echo "<B STYLE=\"line-height:160%;\">Committees:</B><BR>";
     echo "Committee name contains<BR><INPUT TYPE=TEXT NAME=committee STYLE=\"width:95%;\"><HR STYLE=\"width:98%;\">";
@@ -96,8 +96,8 @@
     $javascript_array = "";
     $result = my_query ("SELECT * FROM smry_propositions ORDER BY Target, Election DESC");
     while ($row = $result->fetch_assoc()) {
-      echo "<OPTION VALUE={$row["Election"]}>{$row["Target"]} (" . date ("M j, Y", strtotime ($row["Election"])) . ")</OPTION>";
-      $javascript_array .= "\"" . str_replace ("\"", "'", $row["Target"]) . " (" . date ("M j, Y", strtotime ($row["Election"])) . ")\",";
+      echo "<OPTION VALUE=\"{$row["Target"]}\">{$row["Target"]} (" . date ("M j, Y", strtotime ($row["Election"])) . ")</OPTION>";
+      $javascript_array .= "\"" . str_replace ("\"", "'", $row["Target"]) . "\",";
     }
     return $javascript_array;
   }
@@ -106,7 +106,7 @@
   function fill_election_cycles () {
     $result = my_query ("SELECT ElectionCycle FROM smry_cycles ORDER BY ElectionCycle DESC");
     while ($row = $result->fetch_assoc()) {
-      echo "<INPUT TYPE=CHECKBOX CHECKED> {$row["ElectionCycle"]}&nbsp;&nbsp;&nbsp;&nbsp;";
+      echo "<INPUT NAME=cycles[] TYPE=CHECKBOX VALUE=\"{$row["ElectionCycle"]}\" CHECKED> {$row["ElectionCycle"]}&nbsp;&nbsp;&nbsp;&nbsp;";
     }
   }
 ?>
