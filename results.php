a180408dd1a2aa9353d53638af02727a3a73a4b6
Mike Krejci <mike@maplight.org>
2014-10-09 13:58:35 -0700

    No result table text.

--- results.php
+++ results.php
@@ -1,7 +1,5 @@
 <?php
   function build_results_table () {
-    echo "<TABLE BORDER=1>";
-    echo "<TR><TH>Header 1</TH><TH>Header 2</TH><TH>Header 3</TH><TH>Header 4</TH><TH>Header 5</TH><TH>Header 6</TH><TH>Header 7</TH><TH>Header 8</TH><TH>Header 9</TH></TR>";
-    echo "</TABLE>";
+    echo "<P>&nbsp;</P><B>Search political contributions from 2001 through the present, using the controls on the left.</B>";
   }
 ?>
