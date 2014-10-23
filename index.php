ab54dc298bd194a86b3001c2e9a6c601dec1791a
Mike Krejci <mike@maplight.org>
2014-10-23 10:15:43 -0700

    Set file locations, built header

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<HEAD>
-  <META http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <TITLE>CAPS Project Development</TITLE>
-  <LINK rel="stylesheet" type="text/css" href="main.css"/>
-  <LINK rel="stylesheet" type="text/css" href="caps_style.css"/>
-  <SCRIPT type=text/javascript src=jquery.js></SCRIPT>
-  <SCRIPT type=text/javascript src=jscript.js></SCRIPT>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>California Secretary of State</title>
+  <link rel="stylesheet" href="/css/normalize.css">
+  <link rel="stylesheet" href="/css/main-max-width.css">
+  <noscript><link href="/css/noJs.css" rel="stylesheet" type="text/css" /></noscript>
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="/css/ie-main-max-width.css" media="screen" />
+    <script src="/js/vendor/compatibility.js"></script>
+  <![endif]-->
+  <link rel="stylesheet" type="text/css" href="/css/caps.css" media="all">
+  <script src="/js/vendor/ga-async.js"></script>
+  <script src="/js/jquery.js"></script>
+  <script src="js/caps.js"></script>
 </HEAD>
+
+
 <BODY>
 
 <?php
