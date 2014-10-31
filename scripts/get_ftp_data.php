43ab58acc91e8027500790b57c5f6508ab6297d4
Mike Krejci <mike@maplight.org>
2014-10-31 11:23:33 -0700

    TECHPROJECTS-137 : Debugging mysqli issue with LOAD DATA

--- scripts/get_ftp_data.php
+++ scripts/get_ftp_data.php
@@ -68,14 +68,10 @@
     for ($i = 0; $i < count ($good_tables); $i++) {
       my_query ("TRUNCATE TABLE $good_tables[$i]");
       my_query ("LOAD DATA LOCAL INFILE '" . str_replace ('\\', '/', getcwd ()) . "/{$good_files[$i]}' INTO TABLE {$good_tables[$i]} FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n' IGNORE 1 LINES");
-
-echo "{$my_conn->error}\n";      
-echo ("LOAD DATA LOCAL INFILE '" . str_replace ('\\', '/', getcwd ()) . "/{$good_files[$i]}' INTO TABLE {$good_tables[$i]} FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n' IGNORE 1 LINES\n");
-
-#      unlink ($good_files[$i]);
+      unlink ($good_files[$i]);
     }
   }
 
   # remove all downloaded files
-#  exec ("rm -rf files/*");
+  exec ("rm -rf files/*");
 ?>
