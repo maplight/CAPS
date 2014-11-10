0e98831a2b2cd76847e350a1da0b47c3b2556744
Mike Krejci <mike@maplight.org>
2014-11-10 03:35:55 -0800

    Created a readme file and consolated the images

--- connect.php
+++ connect.php
@@ -1,6 +1,8 @@
 <?php
-  $login = "caps";
-  $pwd = "caps_dev14";
+  $web_login = "caps"; $web_pwd = "caps_dev14";
+  $script_login = "caps_script"; $scripts_pwd = "97_caps_45";
+  $error_email = "mike@maplight.org";
+
 
   $my_conn = mysqli_init ();
   mysqli_options ($my_conn, MYSQLI_OPT_LOCAL_INFILE, true);
