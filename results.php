6e8aacef5e147377d927a83122f9545fd5ba21c6
Mike Krejci <mike@maplight.org>
2014-11-13 06:39:56 -0800

    TECHPROJECTS-158 : Changed how Target gets bolded

--- results.php
+++ results.php
@@ -384,7 +384,7 @@
                 $result2 = my_query ("SELECT Target, COUNT(*) AS TotalCount, SUM(TransactionAmount) AS TotalAmount, SUM(IF(PositionID = 1,1,0)) AS SupportCount, SUM(IF(PositionID=1,TransactionAmount,0)) AS SupportAmount, SUM(IF(PositionID = 2,1,0)) AS OpposeCount, SUM(IF(PositionID=2,TransactionAmount,0)) AS OpposeAmount FROM contributions_search INNER JOIN smry_propositions USING (PropositionID) WHERE Election = '{$election}' AND BallotMeasureContribution = 'Y' GROUP BY Target ORDER BY Target");
                 while ($row2 = $result2->fetch_assoc()) {
                   if (strpos ($row2["Target"], "-") !== false) {
-                    echo "<p><b>" . substr ($row2["Target"], 0, strpos ($row2["Target"], "-")) . "</b>" . substr ($row2["Target"], strpos ($row2["Target"], "-")) . "<br>";
+                    echo "<p><b>" . substr ($row2["Target"], 0, strrpos ($row2["Target"], " - ")) . "</b>" . substr ($row2["Target"], strrpos ($row2["Target"], " - ")) . "<br>";
                   } else {
                     echo "<p><b>{$row2["Target"]}</b><br>";
                   }
