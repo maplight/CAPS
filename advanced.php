52199eec3fbd226b8bfd4ce55623b3c9a1c9a91d
Mike Krejci <mike@maplight.org>
2014-11-25 06:20:57 -0800

    Returned css data

--- advanced.php
+++ advanced.php
@@ -6,6 +6,13 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CAL-ACCESS Campaign Power Search</title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main-max-width.css">
+  <noscript><link href="css/noJs.css" rel="stylesheet" type="text/css" /></noscript>
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="css/ie-main-max-width.css" media="screen" />
+    <script src="js/vendor/compatibility.js"></script>
+  <![endif]-->
   <link rel="stylesheet" type="text/css" href="css/caps.css" media="all">
   <script src="js/vendor/ga-async.js"></script>
   <script src="js/jquery.js"></script>
