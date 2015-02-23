951fa9b86e92e2c8c005accd1979e83e5b8d9a3d
Mike Krejci <mike@maplight.org>
2015-02-23 06:42:01 -0800

    Set up coding changes.

--- scripts/cal_access_data_scraper.php
+++ scripts/cal_access_data_scraper.php
@@ -3,36 +3,36 @@
 
   get_elections_list ();
 
-  $query = "SELECT session FROM cal_access_sessions ORDER BY session DESC LIMIT 1";
-  $result = script_query ($query);
-  while ($row = $result->fetch_assoc()) {
-    $session = $row["session"];
+#  $query = "SELECT session FROM cal_access_sessions ORDER BY session DESC LIMIT 1";
+#  $result = script_query ($query);
+#  while ($row = $result->fetch_assoc()) {
+#    $session = $row["session"];
 
-    get_propositions ($session, 1);
-    get_propositions_committees ($session, 1);
-    get_candidate_names ($session, 1);
-    get_candidate_data ($session, 1);
-  }
+#    get_propositions ($session, 1);
+#    get_propositions_committees ($session, 1);
+#    get_candidate_names ($session, 1);
+#    get_candidate_data ($session, 1);
+#  }
 
   # check for missing proposition committes
   $query = "SELECT cal_access_propositions_committees.session, cal_access_propositions_committees.filer_id FROM cal_access_propositions_committees
             LEFT JOIN cal_access_committees ON (cal_access_committees.filer_id = cal_access_propositions_committees.filer_id AND cal_access_committees.session = cal_access_propositions_committees.session)
             WHERE ISNULL(cal_access_committees.filer_id)";
-  $result = script_query ($query);
-  while ($row = $result->fetch_assoc()) {
-    $session = $row["session"];
-    $filer_id = $row["filer_id"];
-    get_committee_information ($session, $filer_id, 1);
-  }
+#  $result = script_query ($query);
+#  while ($row = $result->fetch_assoc()) {
+#    $session = $row["session"];
+#    $filer_id = $row["filer_id"];
+#    get_committee_information ($session, $filer_id, 1);
+#  }
    
   # check for missing candidate committes
   $query = "SELECT cal_access_candidates_committees.session, cal_access_candidates_committees.filer_id FROM cal_access_candidates_committees
             LEFT JOIN cal_access_committees ON (cal_access_committees.filer_id = cal_access_candidates_committees.filer_id AND cal_access_committees.session = cal_access_candidates_committees.session)
             WHERE ISNULL(cal_access_committees.filer_id)";
-  $result = script_query ($query);
-  while ($row = $result->fetch_assoc()) {
-    $session = $row["session"];
-    $filer_id = $row["filer_id"];
-    get_committee_information ($session, $filer_id, 1);
-  }
+#  $result = script_query ($query);
+#  while ($row = $result->fetch_assoc()) {
+#    $session = $row["session"];
+#    $filer_id = $row["filer_id"];
+#    get_committee_information ($session, $filer_id, 1);
+#  }
 ?>
---
+++ scripts/update_20150223.php
@@ -0,0 +1,18 @@
+<?php
+ALTER TABLE `ca_process`.`cal_access_sessions` 
+ADD COLUMN `run_cycle` ENUM('D','W','M') NULL AFTER `session`,
+ADD COLUMN `last_ran` TIMESTAMP NULL AFTER `run_cycle`;
+
+
+INSERT INTO `ca_process`.`cal_access_sessions` (`session`, `run_cycle`) VALUES ('2015', 'D');
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='D' WHERE `session`='2013';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='W' WHERE `session`='2011';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='M' WHERE `session`='1999';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='M' WHERE `session`='2001';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='M' WHERE `session`='2003';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='M' WHERE `session`='2005';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='M' WHERE `session`='2007';
+UPDATE `ca_process`.`cal_access_sessions` SET `run_cycle`='W' WHERE `session`='2009';
+
+
+?>
