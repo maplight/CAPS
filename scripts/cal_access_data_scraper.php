828478864163bd40272eb93b3ea86285edc2dbe6
Mike Krejci <mike@maplight.org>
2015-02-23 17:23:46 -0800

    Moved script run test to update_data.php

--- scripts/cal_access_data_scraper.php
+++ scripts/cal_access_data_scraper.php
@@ -1,12 +1,6 @@
 <?php
   require_once ("cal_access_scraper.inc");
 
-  # check to see if script is running, do not run if it is
-  $ps_check = trim (shell_exec ("ps aux | grep 'php cal_access_data_scraper.php' | grep -v grep | wc -l"));
-  # If nothing is returned then probably not running in LINUX
-  if ($ps_check == "") {$ps_check = 1;}
-
-
   # Get current day of week, day of month
   $day_of_month = intval (date ("j"));
   switch (date ("w")) {
@@ -20,61 +14,57 @@
   }
   $week_of_month = intval (($day_of_month - 1) / 7) + 1; 
 
+  # Make sure cal_access_elections has all the known elections
+  get_elections_list ();
 
-  # If script isn't currently running, process the data
-  if ($ps_check == "1") {
-    # Make sure cal_access_elections has all the known elections
-    get_elections_list ();
+  # Process data for each session
+  $query = "SELECT * FROM cal_access_sessions ORDER BY session DESC";
+  $result = script_query ($query);
+  while ($row = $result->fetch_assoc()) {
+    $session = $row["session"];
+    $run_days = explode (",", str_replace (" ", "", $row["run_days"]));
 
-    # Process data for each session
-    $query = "SELECT * FROM cal_access_sessions ORDER BY session DESC";
-    $result = script_query ($query);
-    while ($row = $result->fetch_assoc()) {
-      $session = $row["session"];
-      $run_days = explode (",", str_replace (" ", "", $row["run_days"]));
-
-      # Check if the session should run.
-      $process_session = false;
-      foreach ($run_days AS $day) {
-        if (strpos ($day, "-") === false) {
-          if (intval ($day) == $day_of_month || $day == $day_of_week || $day == "") {$process_session = true;}
-        } else {
-          $week = substr ($day, 0, strpos ($day, "-"));
-          $dow = substr ($day, strpos ($day, "-") + 1, 1);
-          if ($week == $week_of_month && $dow = $day_of_week) {$process_session = true;}
-        }
-      }
-
-      # Scrape the session if the schedule is met
-      if ($process_session) {
-        get_propositions ($session, 1);
-        get_propositions_committees ($session, 1);
-        get_candidate_names ($session, 1);
-        get_candidate_data ($session, 1);
-        script_query ("UPDATE cal_access_sessions SET last_ran = NOW() WHERE session = '$session';"); 
+    # Check if the session should run.
+    $process_session = false;
+    foreach ($run_days AS $day) {
+      if (strpos ($day, "-") === false) {
+        if (intval ($day) == $day_of_month || $day == $day_of_week || $day == "") {$process_session = true;}
+      } else {
+        $week = substr ($day, 0, strpos ($day, "-"));
+        $dow = substr ($day, strpos ($day, "-") + 1, 1);
+        if ($week == $week_of_month && $dow = $day_of_week) {$process_session = true;}
       }
     }
 
-    # check for missing proposition committes
-    $query = "SELECT cal_access_propositions_committees.session, cal_access_propositions_committees.filer_id FROM cal_access_propositions_committees
-              LEFT JOIN cal_access_committees ON (cal_access_committees.filer_id = cal_access_propositions_committees.filer_id AND cal_access_committees.session = cal_access_propositions_committees.session)
-              WHERE ISNULL(cal_access_committees.filer_id)";
-    $result = script_query ($query);
-    while ($row = $result->fetch_assoc()) {
-      $session = $row["session"];
-      $filer_id = $row["filer_id"];
-      get_committee_information ($session, $filer_id, 1);
+    # Scrape the session if the schedule is met
+    if ($process_session) {
+      get_propositions ($session, 1);
+      get_propositions_committees ($session, 1);
+      get_candidate_names ($session, 1);
+      get_candidate_data ($session, 1);
+      script_query ("UPDATE cal_access_sessions SET last_ran = NOW() WHERE session = '$session';"); 
     }
+  }
+
+  # check for missing proposition committes
+  $query = "SELECT cal_access_propositions_committees.session, cal_access_propositions_committees.filer_id FROM cal_access_propositions_committees
+            LEFT JOIN cal_access_committees ON (cal_access_committees.filer_id = cal_access_propositions_committees.filer_id AND cal_access_committees.session = cal_access_propositions_committees.session)
+            WHERE ISNULL(cal_access_committees.filer_id)";
+  $result = script_query ($query);
+  while ($row = $result->fetch_assoc()) {
+    $session = $row["session"];
+    $filer_id = $row["filer_id"];
+    get_committee_information ($session, $filer_id, 1);
+  }
    
-    # check for missing candidate committes
-    $query = "SELECT cal_access_candidates_committees.session, cal_access_candidates_committees.filer_id FROM cal_access_candidates_committees
-              LEFT JOIN cal_access_committees ON (cal_access_committees.filer_id = cal_access_candidates_committees.filer_id AND cal_access_committees.session = cal_access_candidates_committees.session)
-              WHERE ISNULL(cal_access_committees.filer_id)";
-    $result = script_query ($query);
-    while ($row = $result->fetch_assoc()) {
-      $session = $row["session"];
-      $filer_id = $row["filer_id"];
-      get_committee_information ($session, $filer_id, 1);
-    }
+  # check for missing candidate committes
+  $query = "SELECT cal_access_candidates_committees.session, cal_access_candidates_committees.filer_id FROM cal_access_candidates_committees
+            LEFT JOIN cal_access_committees ON (cal_access_committees.filer_id = cal_access_candidates_committees.filer_id AND cal_access_committees.session = cal_access_candidates_committees.session)
+            WHERE ISNULL(cal_access_committees.filer_id)";
+  $result = script_query ($query);
+  while ($row = $result->fetch_assoc()) {
+    $session = $row["session"];
+    $filer_id = $row["filer_id"];
+    get_committee_information ($session, $filer_id, 1);
   }
 ?>
