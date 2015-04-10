9ea911faff94b668e0eff33286c33698751b56ba
Mike Krejci <mike@maplight.org>
2015-04-10 10:21:23 -0700

    Code cleaning

--- download_csv.php
+++ download_csv.php
@@ -1,51 +1,51 @@
 <?php
-  require ("connect.php");
+  require("connect.php");
   $where = $_GET["w"];
-  $where_data = unserialize ($_GET["d"]);
-  $sidebar_criteria = unserialize ($_GET["c"]);
-  ksort ($sidebar_criteria);
-  $filename = "data-" . date ("Y-m-d-H-i") . ".csv";
+  $where_data = unserialize($_GET["d"]);
+  $sidebar_criteria = unserialize($_GET["c"]);
+  ksort($sidebar_criteria);
+  $filename = "data-" . date("Y-m-d-H-i") . ".csv";
 
   $search_join = "";
-  if (strpos ($where, "contributions_search_donors") !== false) {$search_join .= "INNER JOIN contributions_search_donors  ON (contributions_search.id = contributions_search_donors.id) ";}
-  if (strpos ($where, "smry_candidates") !== false) {$search_join .= "INNER JOIN smry_candidates USING (MapLightCandidateNameID) ";}
-  if (strpos ($where, "smry_offices") !== false) {$search_join .= "INNER JOIN smry_offices USING (MapLightCandidateOfficeID) ";}
-  if (strpos ($where, "smry_committees") !== false) {$search_join .= "INNER JOIN smry_committees USING (MapLightCommitteeID) ";}
-  if (strpos ($where, "smry_propositions") !== false) {$search_join .= "INNER JOIN smry_propositions USING (PropositionID) ";}
+  if (strpos($where, "contributions_search_donors") !== false) {$search_join .= "INNER JOIN contributions_search_donors  ON (contributions_search.id = contributions_search_donors.id) ";}
+  if (strpos($where, "smry_candidates") !== false) {$search_join .= "INNER JOIN smry_candidates USING (MapLightCandidateNameID) ";}
+  if (strpos($where, "smry_offices") !== false) {$search_join .= "INNER JOIN smry_offices USING (MapLightCandidateOfficeID) ";}
+  if (strpos($where, "smry_committees") !== false) {$search_join .= "INNER JOIN smry_committees USING (MapLightCommitteeID) ";}
+  if (strpos($where, "smry_propositions") !== false) {$search_join .= "INNER JOIN smry_propositions USING (PropositionID) ";}
 
-  $fields = array ("contributions.TransactionType|Transaction Type",
-                   "contributions.ElectionCycle|Cycle",
-                   "contributions.Election|Election",
-                   "contributions.TransactionDateStart|Start Date",
-                   "contributions.TransactionDateEnd|End Date",
-                   "contributions.TransactionAmount|Amount",
-                   "contributions.RecipientCandidateNameNormalized|Recipient Name",
-                   "contributions.RecipientCommitteeNameNormalized|Recipient Committee",
-                   "contributions.RecipientCommitteeID|Recipient Committee ID",
-                   "contributions.RecipientCandidateOffice|Office",
-                   "contributions.RecipientCandidateDistrict|District",
-                   "contributions_grouped.ballot_measures|Ballot Measure(s)",
-                   "contributions.DonorNameNormalized|Contributor Name",
-                   "contributions.DonorCommitteeID|Contributor ID",
-                   "contributions.DonorCity|Contributor City",
-                   "contributions.DonorState|Contributor State",
-                   "contributions.DonorZipCode|Contributor Zip Code",
-                   "contributions.DonorEmployerNormalized|Contributor Employer",
-                   "contributions.DonorOccupationNormalized|Contributor Occupation",
-                   "contributions.CandidateContribution|Candidate Contribution",
-                   "contributions.BallotMeasureContribution|Ballot Measure Contribution",
-                   "contributions.AlliedCommittee|Allied Committee");
+  $fields = array("contributions.TransactionType|Transaction Type",
+                  "contributions.ElectionCycle|Cycle",
+                  "contributions.Election|Election",
+                  "contributions.TransactionDateStart|Start Date",
+                  "contributions.TransactionDateEnd|End Date",
+                  "contributions.TransactionAmount|Amount",
+                  "contributions.RecipientCandidateNameNormalized|Recipient Name",
+                  "contributions.RecipientCommitteeNameNormalized|Recipient Committee",
+                  "contributions.RecipientCommitteeID|Recipient Committee ID",
+                  "contributions.RecipientCandidateOffice|Office",
+                  "contributions.RecipientCandidateDistrict|District",
+                  "contributions_grouped.ballot_measures|Ballot Measure(s)",
+                  "contributions.DonorNameNormalized|Contributor Name",
+                  "contributions.DonorCommitteeID|Contributor ID",
+                  "contributions.DonorCity|Contributor City",
+                  "contributions.DonorState|Contributor State",
+                  "contributions.DonorZipCode|Contributor Zip Code",
+                  "contributions.DonorEmployerNormalized|Contributor Employer",
+                  "contributions.DonorOccupationNormalized|Contributor Occupation",
+                  "contributions.CandidateContribution|Candidate Contribution",
+                  "contributions.BallotMeasureContribution|Ballot Measure Contribution",
+                  "contributions.AlliedCommittee|Allied Committee");
   
   # Build the header and criteria data
   $select_fields = "";
   $header_line = "";
   foreach ($fields as $field) {
-    $field_info = explode ("|", $field);
+    $field_info = explode("|", $field);
     $select_fields .= $field_info[0] . ",";
     $header_line .= "\"" . $field_info[1] . "\",";
   }
-  $select_fields = substr ($select_fields, 0, -1);
-  $header_line = substr ($header_line, 0, -1);
+  $select_fields = substr($select_fields, 0, -1);
+  $header_line = substr($header_line, 0, -1);
 
   # Build the data
   $data = "";
@@ -54,15 +54,15 @@
   foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row) {
     $data_line = "";
     foreach ($row as $value) {
-      $data_line .= "\"" . trim (str_replace ("\"", "\"\"", $value)) . "\",";
+      $data_line .= "\"" . trim(str_replace("\"", "\"\"", $value)) . "\",";
     }
-    $data .= substr ($data_line, 0, -1) . "\n";
+    $data .= substr($data_line, 0, -1) . "\n";
   }
-  $data = str_replace ("\r", "", $data);
+  $data = str_replace("\r", "", $data);
 
   $criteria_data = "\n\"Search criteria used to create this report:\"\n";
   foreach ($sidebar_criteria as $criteria=>$selection) {
-    if ($selection != "") {$criteria_data .= "\"" . substr (str_replace ("_", " ", $criteria), 2) . ": " . $selection . "\"\n";}
+    if ($selection != "") {$criteria_data .= "\"" . substr(str_replace("_", " ", $criteria), 2) . ": " . $selection . "\"\n";}
   }
 
   header("Content-type: application/octet-stream");
@@ -70,4 +70,3 @@
   header("Pragma: no-cache");
   header("Expires: 0");
   echo "$header_line\n$data\n$criteria_data";
-?>
--- scripts/install.php
+++ scripts/install.php
@@ -82,21 +82,4 @@ while ($row = $result->fetch_assoc()) {
 system("php update_data.php");
 
 echo "Install complete....";
-#===============================================================================================
-# load an sql file
-function process_sql_file($filename)
-{
-    $sql_contents = file_get_contents($filename);
-    $sql_contents = rtrim(rtrim($sql_contents), ";");
-    $sql_contents = preg_split("/;(\n|\r)/", $sql_contents);
-
-    foreach ($sql_contents as $query) {
-        if($query){
-            $result = script_query (trim($query));
-            if (!$result)
-                echo "Error on import of " . $query . "\n";
-        }
-    }
-}
-
 ?>
