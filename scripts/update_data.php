7dd992c8ed83c7826b2e58e409f8a25216c9b84d
Mike Krejci <mike@maplight.org>
2015-04-11 11:29:08 -0700

    TECHPROJECTS-237 Completed issue - PDO working

--- scripts/update_data.php
+++ scripts/update_data.php
@@ -13,33 +13,33 @@ if ($ps_check == "1") {
   echo "Starting update... \n";
 
   echo "Update the most recent cal_access session \n";
-#  system("php cal_access_data_scraper.php");
+  system("php cal_access_data_scraper.php");
 
   echo "Get the ftp data \n";
-#  system("php get_ftp_data.php");
+  system("php get_ftp_data.php");
 
   echo "Process data for contributions table - stage 1 \n";
-#  process_sql_file("process_stage_1.sql");
+  process_sql_file("process_stage_1.sql");
 
   echo "Clean up names \n";
   # Clean up names
   clean_candidate_names();
 
   echo "Process data for contributions table - stage 2 \n";
-#  process_sql_file("process_stage_2.sql");
+  process_sql_file("process_stage_2.sql");
 
   echo "Process data for contributions table - stage 3 \n";
-#  process_sql_file("process_stage_3.sql");
+  process_sql_file("process_stage_3.sql");
 
   echo "generate search words \n";
-#  generate_search_words(); 
+  generate_search_words(); 
 
   echo "Reset last update file \n";
-#  $script_db->query("TRUNCATE ca_search.smry_last_update");
-#  $script_db->query("INSERT INTO ca_search.smry_last_update SELECT FiledDate FROM contributions_full WHERE FiledDate <= NOW() ORDER BY FiledDate DESC LIMIT 1");
+  $script_db->query("TRUNCATE ca_search.smry_last_update");
+  $script_db->query("INSERT INTO ca_search.smry_last_update SELECT FiledDate FROM contributions_full WHERE FiledDate <= NOW() ORDER BY FiledDate DESC LIMIT 1");
 
   echo "Process data for contributions table - stage 4 \n";
-#  process_sql_file("process_stage_4.sql");
+  process_sql_file("process_stage_4.sql");
 
   echo "Update done... \n";
 }
