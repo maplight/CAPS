ce580a71a3c1435e2cc20a8a34b729a9cdbd3604
Mike Krejci <mike@maplight.org>
2015-04-11 11:11:24 -0700

    Added special PDO connection for name cleaning query

--- scripts/script_connect.php
+++ scripts/script_connect.php
@@ -3,3 +3,8 @@
   $script_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $script_db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
+  # used for name cleaning prepared statement
+  $script_names_db = new PDO("mysql:host={$hostname};dbname=ca_process;charset=utf8", $script_login, $script_pwd);
+  $script_names_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $script_names_db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
