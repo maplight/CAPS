92893dd3bb28f0487fe0592c34725b53b6691f26
Mike Krejci <mike@maplight.org>
2015-04-12 07:20:00 -0700

    Code cleaning

--- xml/get_committee_list.php
+++ xml/get_committee_list.php
@@ -1,10 +1,10 @@
 <?php
-  require ("../connect.php");
+  require("../connect.php");
 
   $search_text = $_POST["search_text"];
-  $committees = array ();
+  $committees = array();
 
-  $committee_id = intval ($search_text);
+  $committee_id = intval($search_text);
   if ($committee_id == 0) {
     $result = $web_db->prepare("SELECT RecipientCommitteeNameNormalized, RecipientCommitteeID FROM smry_committees WHERE RecipientCommitteeNameNormalized LIKE ? ORDER BY RecipientCommitteeNameNormalized LIMIT 200");
     $result->bindValue(1, "%{$search_text}%", PDO::PARAM_STR);
@@ -14,7 +14,7 @@
     $result->bindValue(2, $committee_id, PDO::PARAM_INT);
   }
   $result->execute();
-  foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row) {$committees[] = $row;}
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {$committees[] = $row;}
 
-  echo json_encode ($committees);
+  echo json_encode($committees);
 
