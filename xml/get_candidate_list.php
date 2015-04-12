0d06fae443c4c069aa4ed400473dd8d0d35a1658
Mike Krejci <mike@maplight.org>
2015-04-12 07:26:56 -0700

    Final code cleanup>

--- xml/get_candidate_list.php
+++ xml/get_candidate_list.php
@@ -2,7 +2,7 @@
   require("../connect.php");
 
   $candidates = array();
-  $result = $web_db->prepare("SELECT DISTINCT RecipientCandidateNameNormalized FROM smry_candidates WHERE UPPER(RecipientCandidateNameNormalized) LIKE UPPER(?) ORDER BY RecipientCandidateNameNormalized LIMIT 50");
+  $result = $web_db->prepare("SELECT RecipientCandidateNameNormalized FROM smry_candidates WHERE UPPER(RecipientCandidateNameNormalized) LIKE UPPER(?) ORDER BY RecipientCandidateNameNormalized LIMIT 50");
   $result->bindValue(1, "%{$_POST["search_text"]}%", PDO::PARAM_STR);
   $result->execute();
   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {$candidates[] = $row;}
