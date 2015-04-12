c2b6c89d08be37e77b7c08e42100e93e30b784fb
Mike Krejci <mike@maplight.org>
2015-04-12 07:25:34 -0700

    Getting distinct names

--- xml/get_committee_list.php
+++ xml/get_committee_list.php
@@ -6,10 +6,10 @@
 
   $committee_id = intval($search_text);
   if ($committee_id == 0) {
-    $result = $web_db->prepare("SELECT RecipientCommitteeNameNormalized, RecipientCommitteeID FROM smry_committees WHERE UPPER(RecipientCommitteeNameNormalized) LIKE UPPER(?) ORDER BY RecipientCommitteeNameNormalized LIMIT 200");
+    $result = $web_db->prepare("SELECT DISTINCT RecipientCommitteeNameNormalized, RecipientCommitteeID FROM smry_committees WHERE UPPER(RecipientCommitteeNameNormalized) LIKE UPPER(?) ORDER BY RecipientCommitteeNameNormalized LIMIT 200");
     $result->bindValue(1, "%{$search_text}%", PDO::PARAM_STR);
   } else {
-    $result = $web_db->prepare("SELECT RecipientCommitteeNameNormalized, RecipientCommitteeID FROM smry_committees WHERE UPPER(RecipientCommitteeNameNormalized) LIKE UPPER(?) OR RecipientCommitteeID = ? ORDER BY RecipientCommitteeNameNormalized LIMIT 200");
+    $result = $web_db->prepare("SELECT DISTINCT RecipientCommitteeNameNormalized, RecipientCommitteeID FROM smry_committees WHERE UPPER(RecipientCommitteeNameNormalized) LIKE UPPER(?) OR RecipientCommitteeID = ? ORDER BY RecipientCommitteeNameNormalized LIMIT 200");
     $result->bindValue(1, "%{$search_text}%", PDO::PARAM_STR);
     $result->bindValue(2, $committee_id, PDO::PARAM_INT);
   }
