afc3dbe3c5cc6fcc84fa5c384230e3ec95d80b72
Mike Krejci <mike@mkrejci.com>
2015-07-30 14:28:30 -0700

    Fixed quick search so that enter works on each search item.

--- index.php
+++ index.php
@@ -35,23 +35,21 @@ require("results.php");
 <div id="caps_wrapper">
     <div id="caps_container">
         <div id="qs_page">
+            <div id="qs_title">
+                <h1 class="font_large_header">CAL-ACCESS Campaign Power Search</h1>
+                <p id="caps_description" class="strong">Search political contributions to candidates, ballot measures, and other committees from 2001 through the present.</p>
+            </div> <!-- end qs_title -->
 
-            <form action="advanced.php" method="post">
-                <input type="hidden" name="quick_search" value="true">
+            <a id="advanced_button" href="advanced.php" class="right font_btn btn_border qs_link1">Go to Advanced Search</a>
 
-                <div id="qs_title">
-                    <h1 class="font_large_header">CAL-ACCESS Campaign Power Search</h1>
-                    <p id="caps_description" class="strong">Search political contributions to candidates, ballot measures, and other committees from 2001 through the present.</p>
-                </div> <!-- end qs_title -->
+            <div id="qs_search">
+                <div class="font_title">Quick Search</div>
+                <hr class="caps_hr1">
 
-                <a id="advanced_button" href="advanced.php" class="right font_btn btn_border qs_link1">Go to Advanced Search</a>
-
-                <div id="qs_search">
-                    <div class="font_title">Quick Search</div>
-                    <hr class="caps_hr1">
-
-                    <img src="img/qs_candidate.jpg" width="50" class="caps_img" alt="Candidate Option Icon">
-                    <div class="qs_box">
+                <img src="img/qs_candidate.jpg" width="50" class="caps_img" alt="Candidate Option Icon">
+                <div class="qs_box">
+                    <form action="advanced.php" method="post">
+                        <input type="hidden" name="quick_search" value="true">
                         <div class="font_title qs_option_title">Candidates</div>
                         <div class="left">How much has&nbsp;</div>
                         <div class="left">
@@ -61,27 +59,34 @@ require("results.php");
                         </div>
                         <div class="left">&nbsp;received?</div><br>
                         <input type="submit" class="qs_btn" name="qs_button" value="Search Candidates">
-                    </div> <!-- end qs_box(Candidates) -->
-                    <hr class="caps_hr1">
+                    </form>
+                </div> <!-- end qs_box(Candidates) -->
+                <hr class="caps_hr1">
 
                     <img src="img/qs_ballot.jpg" width="50" class="caps_img" alt="Ballot Measures Option Icon">
                     <div class="qs_box">
-                        <div class="font_title qs_option_title">Ballot Measures</div>
-                        How much has been raised for all measures on the <select id="propositions_list" name="proposition_list" class="font_input input_border qs_select">
-                            <?php
-                            # Fill election dates dropdown
-                            fill_qs_elections();
-                            ?>
-                        </select> ballot?<br>
-                        <input type="submit" class="qs_btn" name="qs_button" value="Search Ballot Measures">
+                        <form action="advanced.php" method="post">
+                            <input type="hidden" name="quick_search" value="true">
+                            <div class="font_title qs_option_title">Ballot Measures</div>
+                            How much has been raised for all measures on the <select id="propositions_list" name="proposition_list" class="font_input input_border qs_select">
+                                <?php
+                                # Fill election dates dropdown
+                                fill_qs_elections();
+                                ?>
+                            </select> ballot?<br>
+                            <input type="submit" class="qs_btn" name="qs_button" value="Search Ballot Measures">
+                        </form>
                     </div> <!-- end qs_box -->
                     <hr class="caps_hr1">
 
                     <img src="img/qs_contributor.jpg" width="50" class="caps_img" alt="Contributors Option Icon">
                     <div class="qs_box">
-                        <div class="font_title qs_option_title">Contributors</div>
-                        How much has <input type="text" id="contributor" name="contributor" value="company, organization, or person" onFocus="if(this.value == 'company, organization, or person') {this.value = '';}" onBlur="if(this.value == '') {this.value = 'company, organization, or person';}" class="font_input input_border qs_text2" alt="Search Contributors"> contributed?<br>
-                        <input type="submit" class="qs_btn" name="qs_button" value="Search Contributors">
+                        <form action="advanced.php" method="post">
+                            <input type="hidden" name="quick_search" value="true">
+                            <div class="font_title qs_option_title">Contributors</div>
+                            How much has <input type="text" id="contributor" name="contributor" value="company, organization, or person" onFocus="if(this.value == 'company, organization, or person') {this.value = '';}" onBlur="if(this.value == '') {this.value = 'company, organization, or person';}" class="font_input input_border qs_text2" alt="Search Contributors"> contributed?<br>
+                            <input type="submit" class="qs_btn" name="qs_button" value="Search Contributors">
+                        </form>
                     </div> <!-- end qs_box -->
                     <hr class="caps_hr1">
 
