d3d6cbb0b1640379a383172f4a095d41fd9b5510
krejcim <mike@maplight.org>
2015-08-06 19:05:39 +0000

    connect.php edited online with Bitbucket

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
   $web_login = "caps"; $web_pwd = "##_caps_password_##";
   $script_login = "caps_script"; $script_pwd = "##_caps_script_password_##";
-  $error_email = "mike@maplight.org";
+  $error_email = "##_your_error_email_address_##";
   $hostname = "localhost";
 
   $web_db = new PDO("mysql:host={$hostname};dbname=ca_search;charset=utf8", $web_login, $web_pwd);
